deb11d464ab5538a07685f2e4f37ee0a45eb9a7b
Fache Guillaume <gfache@gfache.fr>
2019-04-30 12:22:06 +0200

    new try to run it

--- src/main/java/com/formation/HelloServlet.java
+++ src/main/java/com/formation/HelloServlet.java
@@ -17,11 +17,11 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		  writer.println("Hello from HelloServlet!");
+		writer.println("Hello from HelloServlet!");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
-}
+	}
 }
