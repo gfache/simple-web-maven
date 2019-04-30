28f8053f03558afe3a6fa3362e59ec81e6e9c236
Fache Guillaume <gfache@gfache.fr>
2019-04-30 12:05:54 +0200

    Refactor completly the files to fix structural project issues from the POM

--- src/main/java/com/formation/HelloServlet.java
+++ src/main/java/com/formation/HelloServlet.java
@@ -3,23 +3,25 @@ package com.formation;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.rowset.serial.SerialException;
 
 @WebServlet("/hello")
-public class HelloServlet {
-	
+public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws SerialException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		writer.println("Hello from HelloSerlet!");
+		  writer.println("Hello from HelloServlet!");
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws SerialException, IOException {
-		doGet(request,response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+}
 }
